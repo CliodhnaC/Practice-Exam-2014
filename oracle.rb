b0e12740338fcff00ff80ce418fea6661ac59ec0
CliodhnaC <cliodhna.connolly@ucdconnect.ie>
2015-11-23 23:47:42 +0000

    Commit 1 - Created Files

--- oracle.rb
+++ oracle.rb
@@ -1,5 +1,18 @@
 class Oracle
   def initialize
-    
+    @secret_number = rand(1001)
+  end
+
+  # For testing
+  def to_s
+    "#{@secret_number}"
+  end
+
+  def is_it? (guess)
+    if @secret_number == guess
+      return true
+    end
+    return false
   end
 end
+
--- random_player.rb
+++ random_player.rb
@@ -1,2 +1,17 @@
 class RandomPlayer
+  def initialize (player_name, oracle, range)
+    @player_name = player_name
+    @oracle = oracle
+    @range = range
+  end
+
+  def guess
+    guess = rand(@range)
+    result = oracle.is_it?(guess)
+    if result == true
+      puts "#{@player_name} guessed #{guess} and won!"
+    else
+      puts "#{@player_name} guessed #{guess}"
+    end
+  end
 end
