077be5a754c61fae5fb6fbae964bf66943bb580b
CliodhnaC <cliodhna.connolly@ucdconnect.ie>
2015-11-23 23:52:40 +0000

    Commit 4

--- guess_it.rb
+++ guess_it.rb
@@ -1,2 +1,8 @@
 class GuessIt
+  my_oracle = Oracle.new
+  # ... excluding max, .. including max
+  player_john = RandomPlayer.new("John", my_oracle, (0..1000))
+
+
+  
 end
--- random_player.rb
+++ random_player.rb
@@ -3,15 +3,20 @@ class RandomPlayer
     @player_name = player_name
     @oracle = oracle
     @range = range
+    @result = false
   end
 
   def guess
     guess = rand(@range)
-    result = oracle.is_it?(guess)
+    @result = oracle.is_it?(guess)
     if result == true
       puts "#{@player_name} guessed #{guess} and won!"
     else
       puts "#{@player_name} guessed #{guess}"
     end
+  end
+
+  def has_won?
+    @result
   end
 end
