6671a811b5b53c9cb322f524e80d29bac38717f6
CliodhnaC <cliodhna.connolly@ucdconnect.ie>
2015-11-24 09:52:09 +0000

    Commit 9 - Part 3 works. Is still kinda ugly

--- player.rb
+++ player.rb
@@ -1,9 +1,26 @@
 class Player
+  attr_reader :instruction, :guess
+
   def initialize (player_name, oracle, range)
     @player_name = player_name
     @oracle = oracle
     @range = range
     @result = false
+  end
+
+  def guess
+    random_generator = Random.new
+    @guess = random_generator.rand(@range)
+
+    returned_information = @oracle.is_it?(@guess)
+    @result = returned_information[0]
+    @instruction = returned_information[1]
+
+    if @result == true
+      puts "#{@player_name} guessed #{@guess} and won!"
+    else
+      puts "#{@player_name} guessed #{@guess}"
+    end
   end
 
   def has_won?
--- smart_player.rb
+++ smart_player.rb
@@ -4,24 +4,17 @@ class SmartPlayer < Player
 
   def guess
     puts "Range is: #{@range}"
-    guess = rand(@range)
-
-    returned_information = @oracle.is_it?(guess)
-    @result = returned_information[0]
-    @instruction = returned_information[1]
-
-    if @result == true
-      puts "#{@player_name} guessed #{guess} and won!"
-    else
-      puts "#{@player_name} guessed #{guess}"
-      if returned_information[1] == 'less'
-        min_range = @range.begin
-        @range = min_range..guess
-      else
-        max_range = @range.end
-        @range = guess..max_range
-      end
-    end
+    super()
+    @range = update_range(@range, @instruction, @guess)
   end
 
+  def update_range(range, instruction, guess)
+    if instruction == 'less'
+      min_range = range.begin
+      return range = min_range..guess
+    else
+      max_range = @range.end
+      return range = guess..max_range
+    end
+  end
 end
