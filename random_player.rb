59f88801c7c0f65302c980e3aa5109d3f4cffffe
CliodhnaC <cliodhna.connolly@ucdconnect.ie>
2015-11-24 09:57:34 +0000

    Commit 10 - RandomPlayer empty now

--- random_player.rb
+++ random_player.rb
@@ -2,17 +2,6 @@ require_relative 'player'
 
 class RandomPlayer < Player
 
-  def guess
-    guess = rand(@range)
-
-    returned_information = @oracle.is_it?(guess)
-    @result = returned_information[0]
-    @instruction = returned_information[1]
-
-    if @result == true
-      puts "#{@player_name} guessed #{guess} and won!"
-    else
-      puts "#{@player_name} guessed #{guess}"
-    end
-  end
+  # There's nothing here as RandomPlayer and Player share everything
+  # Might change this so that guess is just defined in each, we see
 end
