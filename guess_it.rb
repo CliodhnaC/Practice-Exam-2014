7c07dd100600af25bddafcc110d79d0526312cff
CliodhnaC <cliodhna.connolly@ucdconnect.ie>
2015-11-24 00:15:00 +0000

    Commit 6 - Part 2 complete and works

--- guess_it.rb
+++ guess_it.rb
@@ -5,15 +5,17 @@ require_relative 'smart_player'
 class GuessIt
   my_oracle = Oracle.new
   # ... excluding max, .. including max
-  #player_john = RandomPlayer.new('John', my_oracle, (0..1000))
+  player_john = RandomPlayer.new('John', my_oracle, (0..1000))
 
+  puts "AND NOW WE HAVE PLAYER ONE!!!!"
   # John's not playing for the minute
-  #while player_john.has_won? == false do
-  #  player_john.guess
-  #end
+  while player_john.has_won? == false do
+    player_john.guess
+  end
 
   player_aoife = SmartPlayer.new('Aoife', my_oracle, (1..1000))
 
+  puts "PLAYER TWO TAKES THE STAGE!!"
   while player_aoife.has_won? == false do
     player_aoife.guess
   end
